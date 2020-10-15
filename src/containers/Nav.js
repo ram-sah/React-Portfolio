13e614aeb3eb4f92f8cbd825cae67bc72594dfc5
Ram Sah <mohan2036@gmail.com>
2020-10-15 06:58:02 -0400

    Added CSS styles of pages

---
+++ src/containers/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../containers/Style.css'
+
+function Nav() {
+    return(
+        <nav className="navbar NavFont">
+            <a className="navbar-brand" style={{ fontSize:'30px', marginLeft: '30px', marginTop: '-10px' }} href="/">                
+               RAM SAH
+            </a>
+            
+        </nav>
+    );
+}
+
+export default Nav;
