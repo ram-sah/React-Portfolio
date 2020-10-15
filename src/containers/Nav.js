b786f5e45b2db3db7101afe59b2493018978df17
Ram Sah <mohan2036@gmail.com>
2020-10-15 07:14:31 -0400

    update link

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -4,7 +4,7 @@ import '../containers/Style.css'
 function Nav() {
     return(
         <nav className="navbar NavFont">
-            <a className="navbar-brand" style={{ fontSize:'30px', marginLeft: '30px', marginTop: '-10px' }} href="/">                
+            <a className="navbar-brand" style={{ fontSize:'30px', marginLeft: '30px', marginTop: '-10px' }} href="#">                
                RAM SAH
             </a>
             
